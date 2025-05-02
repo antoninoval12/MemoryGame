eacc38bf84a044697225bd3b538f405b79e0e52e
antonino_valese <antonino.valese001@studenti.uniparthenope.it>
2025-05-02 19:53:39 +0200

    MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,13 +1,9 @@
 package com.antonino.memorygame
-
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 private const val TAG = "MainActivity"
 

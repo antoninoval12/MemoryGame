56d361a36cf766c79a6891821bd3d09e4026e1af
EmanuelePollastro <emanuele.pollastro001@studenti.uniparthenope.it>
2025-06-11 11:38:07 +0200

    Update MemoryCard.kt

--- MemoryCard.kt
+++ MemoryCard.kt
@@ -1,9 +1,1 @@
-package com.antonino.memorygame
-data class MemoryCard(
-    //Identificativo dell'immagine associata alla carta.
-    val identifier: Int,
-    //Indica se la carta è attualmente scoperta (true) o coperta (false).
-    var isFaceUp: Boolean = false,
-    //Indica se la carta è stata correttamente abbinata con la sua coppia.
-    var isMatched: Boolean = false
-    )
+
